553b4872b2bcb68035a39a7d4cfd387b1df580d4
Finlay Campbell <f.campbell15@imperial.ac.uk>
2017-01-05 17:13:50 +0000

    added function to describe parameter values

--- functions/functions.R
+++ functions/functions.R
@@ -1,2 +1,18 @@
 #===== Loading dependencies and data =====#
 library(ggplot2)
+
+#===== Describe pathogen parameters =====#
+
+make.param <- function() {
+
+    out <- data.frame(matrix(c(
+
+    # | R0  | Mutation rate (/day) | Seq length | Disease  |
+        5   , 1.24e-3/365*(2/3)    , 18058      , # ebola
+        3.5 , 1.14e-5*(2/3)        , 29750      ) # sars
+
+        ,nrow=2,byrow=TRUE,dimnames=list(NULL,c("r0","mut","seql"))))
+
+    out$disease <- c("ebola","sars")
+    out <- out[,c(4,1,2,3)]
+}
