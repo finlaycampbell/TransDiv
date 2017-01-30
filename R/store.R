af5d2b2d05a3d26bf8aedef8f8126ba3e4884a47
Finlay Campbell <f.campbell15@imperial.ac.uk>
2017-01-30 19:47:38 +0000

    included lazy loading of store

---
+++ R/store.R
@@ -0,0 +1,23 @@
+#' Distribution of genetic signatures of common pathogens
+#'
+#' Genetic signatures were simulated using the gensig function for the following
+#' pathogens: Ebola, SARS-CoV, MERS-CoV, Influenza A (H1N1), MRSA, Klebsiella
+#' pneumoniae and Streptococcus pneumoniae. Simulations were run on the DIDE
+#' computer cluster, and the empirical genetic signature of >2000 transmission
+#' pairs per pathogen determined.
+#'
+#' @docType data
+#'
+#' @format A data frame with 32898 rows and 2 columns
+#' \describe{
+#'   \item{disease}{Disease}
+#'   \item{gensig}{Genetic signature of a single transmission event}
+#' }
+#'
+#' @rdname store
+#'
+#' @examples
+#' ## visualise distribution
+#' plot.dist(store)
+#'
+"store"
